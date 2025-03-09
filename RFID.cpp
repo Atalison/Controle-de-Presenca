688b228a3969b66ffa8c7812cacc2b0f282d2a79
Atalison <atalisong.alves@gmail.com>
2025-03-09 12:26:34 -0300

    Texto de exemplo de atulização

---
+++ RFID.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    string in;
+
+    cout << "Escreva sua única palavra aqui: ";
+    cin >> in;
+
+    cout << in;
+
+    return 0;
+}
